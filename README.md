6c2d00733765ec5416faa0c196041290354a7635
Pro696969 <krishnakumaarmc@gmail.com>
2025-03-29 23:41:55 +0530

    basic redis docker img, url mapping

---
+++ README.md
@@ -0,0 +1,5 @@
+###### working basic redis
+
+docker pull redis
+docker run -it --rm --name redis --net redis -p 6969:6379 redis   // redis server
+docker exec -it redis redis-cli                                   // redis client 
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, request
 import pyshorteners
+import redis
+
+# redis server
+REDIS_HOST = "localhost" 
+REDIS_PORT = 6969   
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 app=Flask(__name__)
 
@@ -8,9 +14,11 @@ def home():
     if request.method=="POST":
         url_received=request.form["url"]
         short_url=pyshorteners.Shortener().tinyurl.short(url_received)
+        r.set(short_url, url_received)
+        print(f"URL: {url_received} has been shortened to {short_url}") # debug print
         return render_template("form.html",new_url=short_url,old_url=url_received)
     else:
         return render_template("form.html")
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True, port=5000)
